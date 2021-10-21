87611b9de041765c3407c0a9e12db387fb45b5fd
laura <laura_jankowski@brown.edu>
2021-10-20 21:31:38 -0400

    email works

--- src/pages/contactme.js
+++ src/pages/contactme.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import emailjs from 'emailjs-com';
 
 const ContactMe = () => {
+    function sendEmail(e){
+        e.preventDefault();
+        emailjs.sendForm(
+            'service_qrpghkh',
+            'template_wxab6mf', 
+            e.target, 
+            'user_kmK9IfaOprdkk54lRTNeG'
+            ).then(res=>{
+                console.log(res);
+            }).catch(err=> console.log(err)); 
+        e.target.reset();
+    }
+    
     return (
-        <div style = {{display: 'flex', justifyContent: 'center', alignItems: 'center', height:'90vh'}}>
+        <div className="container border" 
+        style={{marginTop: "50px", 
+                width: '50%', 
+                backgroundImage: `url('https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover'
+                }}>
             <h1>Contact Me</h1>
+            <form onSubmit={sendEmail}>
+                <label>
+                    Name
+                </label>
+                <input type="text" name="name">
+                </input>
+                <label>
+                    Email
+                </label>
+                <input type="email" name="user_email">
+                </input>
+                <label>
+                    Message
+                </label>
+                <textarea name="message" rows="4">
+                </textarea>
+                <input type='submit' value='send'/>
+
+            </form>
         </div>
     )
 }
