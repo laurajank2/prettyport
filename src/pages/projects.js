79933ad504919ecfb7399ac7a040743e3d75fd55
laura <laura_jankowski@brown.edu>
2021-11-03 10:32:09 -0400

    added page size responsiveness to projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,35 +1,34 @@
-import React from 'react';
+import React from "react";
 import { ProjectData } from "./projectData";
 import "./../styles/projects.css";
 
 function Projects() {
-    return (
-        <div className="background">
-            <h1 className="header">Projects</h1>
-            {ProjectData.map((item, index) => {
-              return (
-                
-                <li key={index} className={item.headerCName}>
-                    <h2 className='small-title'>{item.title}</h2>
-                    <div className="row">
-                      <div className="column">
-                        <p>{item.date}</p>
-                        <ul>
-                          <li>{item.info1}</li>
-                          <li>{item.info2}</li>
-                          <li>{item.info3}</li>
-                        </ul>
-                      </div>
-                      <div className="column">
-                        <img src={item.image} width="100%" alt={item.imgAlt}></img>
-                      </div>
-                    </div>
-                </li>
-              );
-            })}
-        </div>
-    )
+  return (
+    <div className="background">
+      <h1 className="header">Projects</h1>
+      {ProjectData.map((item, index) => {
+        return (
+          <li key={index} className={item.headerCName}>
+            <h2 className="small-title">{item.title}</h2>
+
+            <div className="row">
+              <div className="columnimg">
+                <img src={item.image} width="100%" alt={item.imgAlt}></img>
+              </div>
+              <div className="columninfo">
+                <p>{item.date}</p>
+                <ul>
+                  <li>{item.info1}</li>
+                  <li>{item.info2}</li>
+                  <li>{item.info3}</li>
+                </ul>
+              </div>
+            </div>
+          </li>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Projects;
-
