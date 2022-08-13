d7cf6ef506e02513c8455ac7aac222a05cf5b0ad
laurajank2 <67391108+laurajank2@users.noreply.github.com>
2022-08-13 13:42:53 -0700

    Updated Logo link

--- src/pages/projectData.js
+++ src/pages/projectData.js
@@ -5,7 +5,7 @@ export const ProjectData = [
     {
         title: "Meta University Engineering Intern",
         date: "June 2022 - August 2022",
-        image: "https://www.dezeen.com/2021/11/03/meta-facebook-rebrand-infinity-loop/",
+        image: "https://blog.logomyway.com/wp-content/uploads/2021/11/meta-logo.png",
         info1: "Created original iOS application for 7 weeks using Objective C and Xcode",
         info2: "App features include sharing posts and saving them in Parse backend, location sharing, following other users, animations, filters, data management, and more",
         info3: "Also made Twitter, Instagram, and Netflix clones",
