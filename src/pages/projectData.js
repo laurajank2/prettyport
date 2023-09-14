dfc22c4929d54e0e0ae1b665771f23880cace840
laurajank2 <laura_jankowski@brown.edu>
2023-09-14 14:02:14 -0400

    MTF

--- src/pages/projectData.js
+++ src/pages/projectData.js
@@ -1,186 +1,222 @@
-import BlockbusterBudgets from '../photos/BlockbusterBudgets.png'
+import BlockbusterBudgets from "../photos/BlockbusterBudgets.png";
 
 export const ProjectData = [
+  {
+    title: "Math Teaching Fellow",
+    date: "September 2023 - Present",
+    image:
+      "https://www.brown.edu/academics/math/sites/math/files/DOM_2%20Color%20Process%20ST_0.jpg",
+    info1:
+      "Taught Linear Algebra in a section of 64 students and addressing questions during office hours for a total class of 250 students",
+    info2:
+      "Special semester-long certificate program to understand how to be an effective teaching assistant in the Math Department and make mathematics more accessible",
+    info3:
+      "Learned how to enhance growth mindset and equity in mathematics through group discussions, research, and teaching with the professor in the classroom",
+    imgAlt: "SBUDNIC Logo",
+    imgCName: "img",
+    headerCName: "title",
+  },
+  {
+    title: "SBUDNIC",
+    date: "September 2021 - Present",
+    image: "https://sbudnic.netlify.app/static/media/sbudnicN.aeb9041e.png",
+    info1:
+      "Coded software functionality, such as a watchdog timer, in C++ for U3 CubeSat satellite with an Arduino System",
+    info2:
+      "Designed circuit diagrams, soldered, and poured space-grade epoxy for the satellite, which is now in space",
+    info3:
+      "Contributed to research paper for NASA and presented work in person at COSPAR conference in Singapore",
+    imgAlt: "SBUDNIC Logo",
+    imgCName: "img",
+    headerCName: "title",
+  },
 
-    {
-        title: 'SBUDNIC',
-        date: "September 2021 - Present",
-        image: "https://sbudnic.netlify.app/static/media/sbudnicN.aeb9041e.png",
-        info1: "Coded software functionality, such as a watchdog timer, in C++ for U3 CubeSat satellite with an Arduino System",
-        info2: "Designed circuit diagrams, soldered, and poured space-grade epoxy for the satellite, which is now in space",
-        info3: "Contributed to research paper for NASA and presented work in person at COSPAR conference in Singapore",
-        imgAlt: "SBUDNIC Logo",
-        imgCName: "img",
-        headerCName: 'title'
+  {
+    title: "Starbucks Software Engineer Intern",
+    date: "May 2023 - August 2023",
+    image:
+      "https://restaurantclicks.com/wp-content/uploads/2021/10/best-starbucks-drinks.jpg",
+    info1:
+      "Increased Starbucks’ distributed system’ resiliency by changing in-store devices from cloud to edge computing",
+    info2:
+      "Created a Kubernetes cluster with a CoreDNS application that allows in-store devices to communicate locally",
+    info3:
+      "Collaborated with full time engineers to create and design the implementation and presented to Starbucks’ CTO",
+    imgAlt: "kadakareer blurb",
+    imgCName: "img",
+    headerCName: "title",
+  },
 
-    },
-    
-    {
-        title: "Starbucks Software Engineer Intern",
-        date: "May 2023 - August 2023",
-        image: "https://restaurantclicks.com/wp-content/uploads/2021/10/best-starbucks-drinks.jpg",
-        info1: "Increased Starbucks’ distributed system’ resiliency by changing in-store devices from cloud to edge computing",
-        info2: "Created a Kubernetes cluster with a CoreDNS application that allows in-store devices to communicate locally",
-        info3: "Collaborated with full time engineers to create and design the implementation and presented to Starbucks’ CTO",
-        imgAlt: "kadakareer blurb",
-        imgCName: "img",
-        headerCName: 'title'
+  {
+    title: "KadaKareer Project Manager",
+    date: "May 2023 - July 2023",
+    image: "https://www.hbs.edu/news/PublishingImages/KadaKareer2.jpg",
+    info1: "Volunteering as Program Manager for the non-profit KadaKareer",
+    info2:
+      "Managing a team of engineers to make a new platform for career discourse",
+    info3: "Meeting with clients and engineers to conduct the team to success",
+    imgAlt: "kadakareer blurb",
+    imgCName: "img",
+    headerCName: "title",
+  },
 
-    },
-    
-    {
-        title: "KadaKareer Project Manager",
-        date: "May 2023 - July 2023",
-        image: "https://www.hbs.edu/news/PublishingImages/KadaKareer2.jpg",
-        info1: "Volunteering as Program Manager for the non-profit KadaKareer",
-        info2: "Managing a team of engineers to make a new platform for career discourse",
-        info3: "Meeting with clients and engineers to conduct the team to success",
-        imgAlt: "kadakareer blurb",
-        imgCName: "img",
-        headerCName: 'title'
+  {
+    title: "Deep Learning: Mind, Brain, and Machines Final Project",
+    date: "April 2023 - May 2023",
+    image:
+      "https://www.researchgate.net/publication/337771814/figure/fig1/AS:832806989074433@1575568067583/Semantic-segmentation-examples-using-WASPnet.ppm",
+    info1:
+      "Wrote 15 page research proposal to study the relationship between background testing and foreground testing in different AI image recognition systems",
+    info2:
+      "Referenced 23 and read 30+ scientific journal articles about object recognition, types of background training and foreground testing that incorporates various adversarial attacks",
+    info3:
+      "Worked with Professor Thomas Serre, Faculty Director of the Center for Computation & Visualization to refine project proposal and make it ready for research in Fall 2023",
+    imgAlt: "Foreground and background segmentation example",
+    imgCName: "img",
+    headerCName: "title",
+  },
 
-    },
+  {
+    title: "Data Science Final Project: Blockbuster Budgets",
+    date: "February 2023 - May 2023",
+    image: BlockbusterBudgets,
+    info1:
+      "Scraped, cleaned, and analyzed 1000+ item size data set of movies including movie title, budget, gross, release date, and more",
+    info2:
+      "Used machine learning data analysis techniques that we implemented in class such as linear regression and k-means",
+    info3:
+      "Incorporated hypothesis testing to determine whether relationships between movie budget, rating, and gross revenue were significant",
+    imgAlt: "Blockbuster budgets poster",
+    imgCName: "img",
+    headerCName: "title",
+  },
+  {
+    title: "Meta University Engineering Intern",
+    date: "June 2022 - August 2022",
+    image: "https://sdchamber.org/wp-content/uploads/2022/02/Meta-Logo.png",
+    info1:
+      "Created original iOS application for 7 weeks using Objective C and Xcode",
+    info2:
+      "App features include sharing posts and saving them in Parse backend, location sharing, following other users, animations, filters, data management, and more",
+    info3: "Also made Twitter, Instagram, and Netflix clones",
+    imgAlt: "Meta logo",
+    imgCName: "img",
+    headerCName: "title",
+  },
 
-    {
-        title: "Deep Learning: Mind, Brain, and Machines Final Project",
-        date: "April 2023 - May 2023",
-        image: 'https://www.researchgate.net/publication/337771814/figure/fig1/AS:832806989074433@1575568067583/Semantic-segmentation-examples-using-WASPnet.ppm',
-        info1: "Wrote 15 page research proposal to study the relationship between background testing and foreground testing in different AI image recognition systems",
-        info2: "Referenced 23 and read 30+ scientific journal articles about object recognition, types of background training and foreground testing that incorporates various adversarial attacks",
-        info3: "Worked with Professor Thomas Serre, Faculty Director of the Center for Computation & Visualization to refine project proposal and make it ready for research in Fall 2023",
-        imgAlt: "Foreground and background segmentation example",
-        imgCName: "img",
-        headerCName: 'title'
+  {
+    title: "Software Engineering Final Project: Ratty Match",
+    date: "April 2022 - May 2022",
+    image: "https://i.ibb.co/2yGg39t/Screenshot-2023-02-26-at-11-52-09-PM.png",
+    info1:
+      "Worked in team of four to create a website where students can rank dishes at the Brown dining hall, the Ratty",
+    info2:
+      "Featured an algorithm that generated a monthly plan of recommended dishes",
+    info3:
+      "Front end Made with a React, Typescript, HTML, and CSS, and a java backend",
+    imgAlt: "Website homescreen",
+    imgCName: "img",
+    headerCName: "title",
+  },
 
-    },
+  {
+    title: "Student Clinic for Immigrant Justice",
+    date: "November 2021 - April 2022",
+    image:
+      "https://static1.squarespace.com/static/5f09322fafd11c219609f606/t/5f713b9e080c5d3cdcb952a4/1690831285431/",
+    info1:
+      "Volunteering to help build database and user interface for the database",
+    info2: "Working with professional in the field to complete the project",
+    info3:
+      "Organizing database with hundreds of entries with filters and search",
+    imgAlt: "student clinic for immigrant Justice logo",
+    imgCName: "img",
+    headerCName: "title",
+  },
 
-    {
-        title: "Data Science Final Project: Blockbuster Budgets",
-        date: "February 2023 - May 2023",
-        image: BlockbusterBudgets,
-        info1: "Scraped, cleaned, and analyzed 1000+ item size data set of movies including movie title, budget, gross, release date, and more",
-        info2: "Used machine learning data analysis techniques that we implemented in class such as linear regression and k-means",
-        info3: "Incorporated hypothesis testing to determine whether relationships between movie budget, rating, and gross revenue were significant",
-        imgAlt: "Blockbuster budgets poster",
-        imgCName: "img",
-        headerCName: 'title'
+  {
+    title: "Hack@Brown",
+    date: "October 2021 - January 2022",
+    image: "https://i.ibb.co/QHwVrJb/IMG-1344.jpg",
+    info1:
+      "Development Team member for Brown University's Hackathon, Hack@Brown",
+    info2:
+      "Creating website for the hackathon and implementing both front end and back end to the project",
+    info3:
+      "Working in a team of 10 to make sure the website is running for next spring",
+    imgAlt: "Hack@Brown Logo",
+    imgCName: "img",
+    headerCName: "title",
+  },
 
-    },
-    {
-        title: "Meta University Engineering Intern",
-        date: "June 2022 - August 2022",
-        image: "https://sdchamber.org/wp-content/uploads/2022/02/Meta-Logo.png",
-        info1: "Created original iOS application for 7 weeks using Objective C and Xcode",
-        info2: "App features include sharing posts and saving them in Parse backend, location sharing, following other users, animations, filters, data management, and more",
-        info3: "Also made Twitter, Instagram, and Netflix clones",
-        imgAlt: "Meta logo",
-        imgCName: "img",
-        headerCName: 'title'
+  {
+    title: "Terminal Hackathon",
+    date: "September 2021",
+    image:
+      "https://localist-images.azureedge.net/photos/31211567787485/huge/040c8041ea1d28293d63cd9c33e019de401b72ea.jpg",
+    info1:
+      "Earned priority entry into national hackaton through top score on timed computer science assessment",
+    info3: "Terminal Hackathon run by Citadel and Citadel Securities",
+    info2:
+      "Assessment included questions about data structures and knowledge of Java",
+    imgAlt: "terminal hackathon logo",
+    imgCName: "img",
+    headerCName: "name",
+  },
 
-    },  
+  {
+    title: "Graph",
+    date: "July 2021 - August 2021",
+    image: "https://i.ibb.co/n7jvqsr/Screen-Shot-2021-10-31-at-6-11-08-PM.jpg",
+    info1: "Final Project for Data Structures and Algorithms",
+    info2:
+      "Created updatable node system and implemented PageRank and Prim-Jarnik algorithms",
+    info3:
+      "Wrote exhausitve testing suite to ensure the programs ran correctly",
+    imgAlt: "Photo of my graph final project",
+    imgCName: "img",
+    headerCName: "title",
+  },
 
-    {
-        title: "Software Engineering Final Project: Ratty Match",
-        date: "April 2022 - May 2022",
-        image:"https://i.ibb.co/2yGg39t/Screenshot-2023-02-26-at-11-52-09-PM.png",
-        info1: "Worked in team of four to create a website where students can rank dishes at the Brown dining hall, the Ratty",
-        info2: "Featured an algorithm that generated a monthly plan of recommended dishes",
-        info3: "Front end Made with a React, Typescript, HTML, and CSS, and a java backend",
-        imgAlt: "Website homescreen",
-        imgCName: "img",
-        headerCName: 'title'
+  {
+    title: "Pacman",
+    image:
+      "https://thereader.mitpress.mit.edu/wp-content/uploads/2020/11/pacman-lead-graphic-700x420.jpg",
+    date: "May 2021 - June 2021",
+    info1: "Final Project for Object Oriented Programming",
+    info2:
+      "Created Pacman game using object oriented programing techniques in Java",
+    info3:
+      "Implemented breadth-first-search AI algorithm to create ghost chase functionality",
+    imgAlt: "Photo of my version of pacman",
+    imgCName: "img",
+    headerCName: "title",
+  },
 
-    }, 
+  // {
+  //     title: 'Tetris',
+  //     image: "https://lh3.googleusercontent.com/inMkUY9jWZYWar2aCi5VbSLKieWOnt3zQ3mdVbx1POcgFC1fkutuTUDPpV9YluAJeZcIBM-cTNFJV3E4pMHGSMsD0Ra9JnCPBlXvACuaDqC3F19BaYqeIBxsZwRrmkH6kw=w1280",
+  //     date: "May 2021",
+  //     info1: "Midterm Project for Object Oriented Programming",
+  //     info2: "Created Tetris game completely from scratch using Java",
+  //     info3: "Implemented multiple levels and scoring system and extra functionaility for the game",
+  //     imgAlt: "Photo of my version of tetris",
+  //     imgCName: "img",
+  //     headerCName: 'title'
 
-    {
-        title: 'Student Clinic for Immigrant Justice',
-        date: "November 2021 - April 2022",
-        image: "https://static1.squarespace.com/static/5f09322fafd11c219609f606/t/5f713b9e080c5d3cdcb952a4/1690831285431/",
-        info1: "Volunteering to help build database and user interface for the database",
-        info2: "Working with professional in the field to complete the project",
-        info3: "Organizing database with hundreds of entries with filters and search",
-        imgAlt: "student clinic for immigrant Justice logo",
-        imgCName: "img",
-        headerCName: 'title'
+  // },
 
-    },  
-
-    {
-        title: 'Hack@Brown',
-        date: "October 2021 - January 2022",
-        image: "https://i.ibb.co/QHwVrJb/IMG-1344.jpg",
-        info1: "Development Team member for Brown University's Hackathon, Hack@Brown",
-        info2: "Creating website for the hackathon and implementing both front end and back end to the project",
-        info3: "Working in a team of 10 to make sure the website is running for next spring",
-        imgAlt: "Hack@Brown Logo",
-        imgCName: "img",
-        headerCName: 'title'
-
-    }, 
-
-    {
-        title: "Terminal Hackathon",
-        date: "September 2021",
-        image:
-          "https://localist-images.azureedge.net/photos/31211567787485/huge/040c8041ea1d28293d63cd9c33e019de401b72ea.jpg",
-        info1:
-          "Earned priority entry into national hackaton through top score on timed computer science assessment",
-        info3: "Terminal Hackathon run by Citadel and Citadel Securities",
-        info2: "Assessment included questions about data structures and knowledge of Java",
-        imgAlt: "terminal hackathon logo",
-        imgCName: "img",
-        headerCName: "name",
-      },
-    
-    {
-        title: 'Graph',
-        date: "July 2021 - August 2021",
-        image: "https://i.ibb.co/n7jvqsr/Screen-Shot-2021-10-31-at-6-11-08-PM.jpg",
-        info1: "Final Project for Data Structures and Algorithms",
-        info2: "Created updatable node system and implemented PageRank and Prim-Jarnik algorithms",
-        info3: "Wrote exhausitve testing suite to ensure the programs ran correctly",
-        imgAlt: "Photo of my graph final project",
-        imgCName: "img",
-        headerCName: 'title'
-
-    }, 
-
-    {
-        title: 'Pacman',
-        image: "https://thereader.mitpress.mit.edu/wp-content/uploads/2020/11/pacman-lead-graphic-700x420.jpg",
-        date: "May 2021 - June 2021",
-        info1: "Final Project for Object Oriented Programming",
-        info2: "Created Pacman game using object oriented programing techniques in Java",
-        info3: "Implemented breadth-first-search AI algorithm to create ghost chase functionality",
-        imgAlt: "Photo of my version of pacman",
-        imgCName: "img",
-        headerCName: 'title'
-
-    }, 
-
-    // {
-    //     title: 'Tetris',
-    //     image: "https://lh3.googleusercontent.com/inMkUY9jWZYWar2aCi5VbSLKieWOnt3zQ3mdVbx1POcgFC1fkutuTUDPpV9YluAJeZcIBM-cTNFJV3E4pMHGSMsD0Ra9JnCPBlXvACuaDqC3F19BaYqeIBxsZwRrmkH6kw=w1280",
-    //     date: "May 2021",
-    //     info1: "Midterm Project for Object Oriented Programming",
-    //     info2: "Created Tetris game completely from scratch using Java",
-    //     info3: "Implemented multiple levels and scoring system and extra functionaility for the game",
-    //     imgAlt: "Photo of my version of tetris",
-    //     imgCName: "img",
-    //     headerCName: 'title'
-
-    // }, 
-
-    {
-        title: 'Nirox Arts',
-        image: "https://i.ibb.co/x21jJs1/Screen-Shot-2021-10-28-at-7-35-26-PM.png",
-        date: "June 2020 - August 2020",
-        info1: "Developed web applications using HTML 5, CSS 3 and Javascript/Typescript with one partner",
-        info2: "Implemented full-stack applications using React and modeled, retrieved, and stored data in Firebase(remote cloud database)",
-        info3: "Integrated various APIs and Libraries (Bootcamp) into the website",
-        imgCName: "img",
-        imgAlt: "Photo of NiroxArts website",
-        headerCName: 'title'
-
-    }, 
-]
+  {
+    title: "Nirox Arts",
+    image: "https://i.ibb.co/x21jJs1/Screen-Shot-2021-10-28-at-7-35-26-PM.png",
+    date: "June 2020 - August 2020",
+    info1:
+      "Developed web applications using HTML 5, CSS 3 and Javascript/Typescript with one partner",
+    info2:
+      "Implemented full-stack applications using React and modeled, retrieved, and stored data in Firebase(remote cloud database)",
+    info3: "Integrated various APIs and Libraries (Bootcamp) into the website",
+    imgCName: "img",
+    imgAlt: "Photo of NiroxArts website",
+    headerCName: "title",
+  },
+];
